3316a6461bd7355faf1a49ed3b92d483353dd158
Dheeraj Yalamanchili <dhee9000@gmail.com>
2020-02-29 02:19:33 -0600

    resolved voice chat issue

--- src/react/matching/Matching.js
+++ src/react/matching/Matching.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, NativeModules } from 'react-native';
+import { View, Button, Dimensions, NativeModules } from 'react-native';
 
 import { Text } from '../common/components';
 
@@ -21,12 +21,70 @@ import { connect } from 'react-redux';
 import { ActionTypes } from '../../redux/';
 import { Colors, Fonts } from '../../config';
 
-class Start extends React.Component{
+const {width, height} = Dimensions.get('screen');
+
+class Matching extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            peerIds: [],                                //Array for storing connected peers
+            uid: Math.floor(Math.random() * 100),       //Generate a UID for local user
+            appid: AgoraConfig.AppID,                    //Enter the App ID generated from the Agora Website
+            channelName: "TestRoom",        //Channel Name for the current session
+            vidMute: false,                             //State variable for Video Mute
+            audMute: false,                             //State variable for Audio Mute
+            joinSucceed: false,                         //State variable for storing success
+        };
+
+        const config = {                            //Setting config of the app
+            appid: this.state.appid,                  //App ID
+            channelProfile: 0,                        //Set channel profile as 0 for RTC
+            videoEncoderConfig: {                     //Set Video feed encoder settings
+            width: 720,
+            height: 1080,
+            bitrate: 1, 
+            frameRate: FPS30,
+            orientationMode: Adaptative,
+            },
+            audioProfile: AudioProfileDefault,
+            audioScenario: AudioScenarioDefault,
+        };
+        RtcEngine.init(config);                     //Initialize the RTC engine
+    }
 
     componentDidMount(){
         Permissions.askAsync(Permissions.AUDIO_RECORDING,);
+
+        RtcEngine.on('userJoined', (data) => {
+            const { peerIds } = this.state;             //Get currrent peer IDs
+            if (peerIds.indexOf(data.uid) === -1) {     //If new user has joined
+              this.setState({
+                peerIds: [...peerIds, data.uid],        //add peer ID to state array
+              });
+            }
+        });
+        RtcEngine.on('userOffline', (data) => {       //If user leaves
+            this.setState({
+                peerIds: this.state.peerIds.filter(uid => uid !== data.uid), //remove peer ID from state array
+            });
+        });
+        RtcEngine.on('joinChannelSuccess', (data) => {                   //If Local user joins RTC channel
+            RtcEngine.startPreview();                                      //Start RTC preview
+            this.setState({
+                joinSucceed: true,                                           //Set state variable to true
+            });
+        });    
     }
-    
+
+    startCall() {
+        RtcEngine.joinChannel(this.state.channelName, this.state.uid);  //Join Channel
+        RtcEngine.enableAudio();
+    }
+
+    endCall() {
+        RtcEngine.leaveChannel();
+    }
       
     render(){
         return(
@@ -35,11 +93,21 @@ class Start extends React.Component{
                 <Button 
                     title={'Connect To Room'} onPress={
                         () => {
-
+                            this.startCall();
                         }
                     } 
                 />
-                <Text>{this.state.status}</Text>
+                <Button 
+                    title={'Connect To Room'} onPress={
+                        () => {
+                            this.endCall();
+                        }
+                    } 
+                />
+                <View style={{height: height*3/4}}>
+                <AgoraView style={{ flex: 1 }}
+                  remoteUid={this.state.peerIds[0]} mode={1} key={this.state.peerIds[0]} />
+                </View>
             </View>
         )
     }
@@ -53,4 +121,4 @@ const mapDispatchToProps = dispatch => ({
 
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Start);
+export default connect(mapStateToProps, mapDispatchToProps)(Matching);
