fa1d7f4506b2c5f2ecb8a78cf3af33f9eb22f15e
dhee9000 <dhee9000@gmail.com>
2020-06-23 18:54:55 -0500

    added tabbed view for Matches and Chats

--- src/react/chat/Chats.js
+++ src/react/chat/Chats.js
@@ -15,6 +15,8 @@ import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
 import auth from '@react-native-firebase/auth';
 
+import Animated from 'react-native-reanimated';
+
 const BLANK_IMAGE_URI = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
 
 generateCombinedDocId = function (uid1, uid2) {
@@ -32,9 +34,113 @@ generateCombinedDocId = function (uid1, uid2) {
 class Chats extends React.Component {
 
     state = {
-        chats: [],
+        tabIdx: 0,
+        routes: [
+            { key: 'matches', title: 'matches' },
+            { key: 'chats', title: 'chats' },
+        ]
+    }
+
+    renderTabScene = SceneMap({
+        matches: ConnectedMatchesView,
+        chats: ConnectedChatsView,
+    })
+
+    _renderTabBar = props => {
+        const inputRange = props.navigationState.routes.map((x, i) => i);
+
+        return (
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                {props.navigationState.routes.map((route, i) => {
+                    const color = Animated.color(
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 255 : 230
+                                ),
+                            })
+                        ),
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 255 : 52
+                                ),
+                            })
+                        ),
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 255 : 98
+                                ),
+                            })
+                        )
+                    );
+
+                    const backgroundColor = Animated.color(
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 230 : 255
+                                ),
+                            })
+                        ),
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 52 : 255
+                                ),
+                            })
+                        ),
+                        Animated.round(
+                            Animated.interpolate(props.position, {
+                                inputRange,
+                                outputRange: inputRange.map(inputIndex =>
+                                    inputIndex === i ? 98 : 255
+                                ),
+                            })
+                        )
+                    );
+
+                    return (
+                        <TouchableOpacity
+                            key={route.key}
+                            onPress={() => this.setState({ tabIdx: i })}>
+                            <Animated.View style={{ backgroundColor, paddingVertical: 4.0, paddingHorizontal: 8.0, borderRadius: 24.0 }}>
+                                <Animated.Text style={{ color, fontSize: 16.0, fontFamily: Fonts.heading }}>{route.title}</Animated.Text>
+                            </Animated.View>
+                        </TouchableOpacity>
+                    );
+                })}
+            </View>
+        );
+    };
+
+
+
+    render() {
+        return (
+            <View style={{ backgroundColor: Colors.background, flex: 1, paddingTop: 32.0 }}>
+                <TabView
+                    navigationState={{ index: this.state.tabIdx, routes: this.state.routes }}
+                    renderScene={this.renderTabScene}
+                    renderTabBar={this._renderTabBar}
+                    onIndexChange={idx => this.setState({ tabIdx: idx })}
+                    swipeEnabled={false}
+                />
+            </View>
+        )
+    }
+}
+
+class MatchesView extends React.Component {
+
+    state = {
         matches: [],
-        matchesToDisplay: [],
     }
 
     componentDidMount() {
@@ -44,21 +150,6 @@ class Chats extends React.Component {
         firestore().collection('matches').where('uids', 'array-contains', auth().currentUser.uid).onSnapshot(snapshot => {
             this.onMatchesUpdated(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
         });
-
-        // Chats Listeners
-        firestore().collection('chats').where('uids', 'array-contains', auth().currentUser.uid).onSnapshot(snapshot => {
-            this.onChatsUpdated(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-        });
-    }
-
-    onChatsUpdated = chats => {
-        chats = chats.map(chat => {
-            let otherUid = chat.uids.filter(uid => uid != auth().currentUser.uid)[0];
-            return { ...chat, uid: otherUid }
-        })
-
-        console.log(chats);
-        this.setState({ chats });
     }
 
     onMatchesUpdated = async matchesData => {
@@ -91,10 +182,6 @@ class Chats extends React.Component {
         });
     }
 
-    chatClicked = (chatId, userId) => {
-        this.props.navigation.navigate('ChatView', {chatId, userId});
-    }
-
     renderMatch = ({ item }) => {
 
         if (this.props.profilesById[item.uid].loaded) {
@@ -115,20 +202,66 @@ class Chats extends React.Component {
         }
     }
 
+    render() {
+        return (
+            <View style={{paddingTop: 16.0}}>
+                <FlatList
+                    horizontal
+                    data={this.state.matches.filter(match => this.state.chats.find(chat => chat.uids.includes(match.uids.filter(uid => uid != auth().currentUser.uid)[0])) === undefined)}
+                    renderItem={this.renderMatch}
+                    keyExtractor={item => item.id}
+                    ListEmptyComponent={<Text style={{ color: Colors.textLightGray, alignSelf: 'center', textAlign: 'center', marginHorizontal: 16.0 }}>No matches found.</Text>}
+                />
+            </View>
+        )
+    }
+}
+
+class ChatsView extends React.Component {
+
+    state = {
+        chats: [],
+    }
+
+    componentDidMount() {
+        // TODO: Remove this line
+        this.props.getProfile(auth().currentUser.uid);
+
+        // Chats Listeners
+        firestore().collection('chats').where('uids', 'array-contains', auth().currentUser.uid).onSnapshot(snapshot => {
+            this.onChatsUpdated(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+        });
+    }
+
+    onChatsUpdated = chats => {
+        chats = chats.map(chat => {
+            let otherUid = chat.uids.filter(uid => uid != auth().currentUser.uid)[0];
+            return { ...chat, uid: otherUid }
+        })
+
+        console.log(chats);
+        this.setState({ chats });
+    }
+
+    chatClicked = (chatId, userId) => {
+        this.props.navigation.navigate('ChatView', { chatId, userId });
+    }
+
+
     renderChat = ({ item }) => {
         if (this.props.profilesById[item.uid].loaded) {
             let profile = this.props.profilesById[item.uid];
             return (
                 <TouchableOpacity onPress={() => this.chatClicked(item.id, item.uid)}>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 8.0 }}>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                             <Image source={{ uri: profile.images["1"] ? profile.images["1"].url : BLANK_IMAGE_URI }} style={{ borderRadius: 16, height: 32, width: 32, }} />
                             <View style={{ justifyContent: 'center', alignItems: 'flex-start', padding: 8.0 }}>
                                 <Text numberOfLines={2} style={{ fontSize: 16.0, textAlign: 'center' }}>{profile.fname} {profile.lname}</Text>
                                 <Text style={{ fontSize: 10.0 }}>{item.lastMessageBy == auth().currentUser.uid ? 'You' : profile.fname}: {item.lastMessage}</Text>
                             </View>
                         </View>
-                        <Text style={{ fontSize: 16.0, color: Colors.textLightGray,}}>1d</Text>
+                        <Text style={{ fontSize: 16.0, color: Colors.textLightGray, }}>1d</Text>
                     </View>
                 </TouchableOpacity>
             )
@@ -142,27 +275,13 @@ class Chats extends React.Component {
 
     render() {
         return (
-            <View style={{ backgroundColor: Colors.background, flex: 1 }}>
-                <View style={{ paddingTop: 16.0, paddingLeft: 16.0 }}>
-                    <Text style={{ fontFamily: Fonts.heading, fontSize: 32.0 }}>Chats</Text>
-                </View>
-                <View>
-                    <FlatList
-                        horizontal
-                        data={this.state.matches.filter(match => this.state.chats.find(chat => chat.uids.includes(match.uids.filter(uid => uid != auth().currentUser.uid)[0])) === undefined)}
-                        renderItem={this.renderMatch}
-                        keyExtractor={item => item.id}
-                        ListEmptyComponent={<Text style={{ color: Colors.textLightGray, alignSelf: 'center', textAlign: 'center', marginHorizontal: 16.0 }}>No matches found.</Text>}
-                    />
-                </View>
-                <View>
-                    <FlatList
-                        ListEmptyComponent={<Text style={{ color: Colors.textLightGray, alignSelf: 'center', textAlign: 'center', marginHorizontal: 16.0 }}>No chats found. Start matching to find people to chat with!</Text>}
-                        data={this.state.chats}
-                        keyExtractor={item => item.id}
-                        renderItem={this.renderChat}
-                    />
-                </View>
+            <View style={{paddingTop:16.0}}>
+                <FlatList
+                    ListEmptyComponent={<Text style={{ color: Colors.textLightGray, alignSelf: 'center', textAlign: 'center', marginHorizontal: 16.0 }}>No chats found. Start matching to find people to chat with!</Text>}
+                    data={this.state.chats}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderChat}
+                />
             </View>
         )
     }
@@ -176,5 +295,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     getProfile: uid => dispatch({ type: ActionTypes.FETCH_PROFILE.REQUEST, payload: uid }),
 });
+
+const ConnectedMatchesView = connect(mapStateToProps, mapDispatchToProps)(MatchesView);
+const ConnectedChatsView = connect(mapStateToProps, mapDispatchToProps)(ChatsView);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chats);
