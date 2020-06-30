dc1f0e9862c3afae6ba15ebc3440a76cf8705822
dhee9000 <dhee9000@gmail.com>
2020-06-30 03:50:31 -0500

    moved tab bar into common

---
+++ src/react/common/components/TabBar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import RNAnimated from 'react-native-reanimated';
+import { Fonts, Colors } from '../../../config';
+
+const TabBar = props => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
+            {props.navigationState.routes.map((route, i) => {
+                const color = RNAnimated.color(
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 255 : 230
+                            ),
+                        })
+                    ),
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 255 : 52
+                            ),
+                        })
+                    ),
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 255 : 98
+                            ),
+                        })
+                    )
+                );
+
+                const backgroundColor = RNAnimated.color(
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 230 : 255
+                            ),
+                        })
+                    ),
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 52 : 255
+                            ),
+                        })
+                    ),
+                    RNAnimated.round(
+                        RNAnimated.interpolate(props.position, {
+                            inputRange,
+                            outputRange: inputRange.map(inputIndex =>
+                                inputIndex === i ? 98 : 255
+                            ),
+                        })
+                    )
+                );
+
+                return (
+                    <TouchableOpacity
+                        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                        key={route.key}
+                        onPress={() => props.onChangeTab(i)}>
+                        <RNAnimated.View style={{ backgroundColor, paddingVertical: 4.0, paddingHorizontal: 8.0, borderRadius: 24.0 }}>
+                            <RNAnimated.Text style={{ color, fontSize: 16.0, fontFamily: Fonts.heading }}>{route.title}</RNAnimated.Text>
+                        </RNAnimated.View>
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    );
+};
+
+export default TabBar;
--- src/react/common/components/index.js
+++ src/react/common/components/index.js
@@ -1,3 +1,4 @@
 export {default as Text} from './Text';
 export {default as DismissKeyboardView} from './DismissKeyboardView'
 export { default as RadioButton } from './RadioButton';
+export { default as TabBar } from './TabBar';
