bd50d298b7e042bfbadca8aa7bcbae9b51cac043
dhee9000 <dhee9000@gmail.com>
2020-06-30 03:52:13 -0500

    added reducers and sagas for chats and messages

---
+++ src/redux/chats/reducers/ChatsReducer.js
@@ -0,0 +1,61 @@
+import { combineReducers } from 'redux';
+import * as ActionTypes from '../../ActionTypes';
+
+const allIds = (state = [], action = {}) => {
+    switch (action.type) {
+        case ActionTypes.FETCH_MATCH.REQUEST: {
+
+            let uid = action.payload;
+            if (!state.includes(uid)) {
+                state.push(uid);
+            }
+
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+const byId = (state = {}, action = {}) => {
+    switch (action.type) {
+        case ActionTypes.FETCH_MATCH.REQUEST: {
+
+            let uid = action.payload;
+            if (!state[uid]) {
+                return {
+                    ...state,
+                    [uid]: {
+                        loaded: false,
+                        loading: true,
+                        error: false,
+                    }
+                }
+            }
+
+        }
+        
+        case ActionTypes.FETCH_MATCH.SUCCESS: {
+
+            let data = action.payload;
+
+            return {
+                ...state,
+                [data.id]: {
+                    loaded: true,
+                    loading: false,
+                    error: false,
+                    ...data,
+                }
+            }
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
+
+const ProfilesReducer = combineReducers({allIds, byId});
+
+export default ProfilesReducer;
---
+++ src/redux/matches/sagas/index.js
@@ -0,0 +1,1 @@
+export { default as MatchesSaga } from './MatchesSaga';
