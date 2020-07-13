d466fca5483bb09a0a02d4c4ba21b77e09d60a7e
dhee9000 <dhee9000@gmail.com>
2020-07-13 03:57:13 -0500

    added location library to app

--- src/config/index.js
+++ src/config/index.js
@@ -3,3 +3,4 @@ export { default as Colors } from './Colors';
 export { default as Firebase } from './Firebase';
 export { default as AgoraConfig } from './Agora';
 export { default as ApolloClient} from './Apollo';
+export { default as GoogleMaps} from './GoogleMaps';
