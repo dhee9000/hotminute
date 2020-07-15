c5b03a66fdc9b696e1c41462518356600b8b69e3
dhee9000 <dhee9000@gmail.com>
2020-07-15 16:06:11 -0500

    added countdown timer for dating periods

--- src/react/splash/DatingPeriodInfo.js
+++ src/react/splash/DatingPeriodInfo.js
@@ -4,13 +4,24 @@ import { View, Image, Animated } from 'react-native';
 import { Text } from '../common/components';
 import { Fonts, Colors } from '../../config';
 
+import firestore from '@react-native-firebase/firestore';
+
 import Heart from '../../../assets/svg/heart.svg';
 
 import { LinearGradient } from 'expo-linear-gradient';
 
+import CountDown from 'react-native-countdown-component';
+
 class DatingPeriodInfo extends React.Component {
 
-    componentDidMount() {
+    state={
+        inDatingPeriod: false,
+        datingPeriods: [],
+        datingPeriodLength: 0,
+        timeToNext: 10,
+    }
+
+    async componentDidMount() {
         Animated.loop(
             Animated.timing(this.loadingAnim,
                 {
@@ -20,6 +31,36 @@ class DatingPeriodInfo extends React.Component {
                 }
             )
         ).start();
+
+        let configDocSnapshot = await firestore().collection('meta').doc('config').get();
+        let { datingPeriods, datingPeriodLength } = configDocSnapshot.data();
+        datingPeriods = datingPeriods.map(period => period.toDate());
+
+        let inDatingPeriod = false;
+        let nextDatingPeriod = new Date(2524715940000);
+        let currentHour = new Date().getUTCHours();
+
+        datingPeriods.map(datingPeriod => {
+            if (datingPeriod.getUTCHours() <= currentHour && datingPeriod.getUTCHours() + datingPeriodLength / 60 >= currentHour) {
+                inDatingPeriod = inDatingPeriod || true;
+            }
+            else {
+                inDatingPeriod = inDatingPeriod || false;
+            }
+
+            if(datingPeriod.getUTCHours() - nextDatingPeriod.getUTCHours() < 0 && nextDatingPeriod.getUTCHours() > currentHour){
+                console.log("CHECK")
+                nextDatingPeriod = new Date(datingPeriod.getTime());
+            }
+        });
+
+        if(nextDatingPeriod.getTime() == new Date(2524715940000).getTime()){
+            nextDatingPeriod = datingPeriods[0];
+        }
+
+        let timeToNext = Math.max((nextDatingPeriod.getUTCHours() - currentHour)*60*60, (currentHour - nextDatingPeriod.getUTCHours())*60*60);
+        console.log(timeToNext);
+        this.setState({timeToNext, inDatingPeriod, datingPeriods, datingPeriodLength});
     }
 
     loadingAnim = new Animated.Value(0);
@@ -31,19 +72,16 @@ class DatingPeriodInfo extends React.Component {
                     <Text style={{ fontFamily: Fonts.heading, fontSize: 18 }}>the time isn't quite right...</Text>
                     <Text>introducing</Text>
                     <Text style={{ fontSize: 32.0 }}>Dating Periods</Text>
-                    <Animated.View source={require('../../../assets/img/logo.png')} style={
-                        {
-                            transform: [
-                                { scale: this.loadingAnim.interpolate({ inputRange: [0, 0.5, 1], outputRange: [1, 0.8, 1] }) }
-                            ], opacity: this.enterAnim
-                        }
-                    }>
+                    <Animated.View source={require('../../../assets/img/logo.png')} style={{ transform: [{ scale: this.loadingAnim.interpolate({ inputRange: [0, 0.5, 1], outputRange: [1, 0.8, 1] }) }], opacity: this.enterAnim }}>
                         <Heart height={144} width={144} />
                     </Animated.View>
-                    <Text>hotminute needs your potential matches to be online at the same time as you so that you can chat with them!</Text>
-                    <Image source={{ uri: 'https://www.nationalgeographic.com/content/dam/science/2019/03/08/daylight-saving/dst5.gif' }} style={{ height: 120, width: 120, borderRadius: 8.0, margin: 16.0 }} />
-                    <Text>That's why we have Dating Periods. Check back at these special times of the day to find your match!</Text>
-                    <Image source={{ uri: 'https://media1.giphy.com/media/ZvFsqDaaOLkSQ/giphy.gif' }} style={{ height: 120, width: 120, borderRadius: 8.0, margin: 16.0 }} />
+                    <CountDown
+                        until={this.state.timeToNext}
+                        size={24}
+                        running={true}
+                    />
+                    <Text style={{ textAlign: 'center' }}>hotminute needs your potential matches to be online at the same time as you so that you can chat with them!</Text>
+                    <Text style={{ textAlign: 'center' }}>That's why we have Dating Periods. Check back at these special times of the day to find your match!</Text>
                 </LinearGradient>
             </View>
         )
