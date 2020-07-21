ee8a77b16db898b29236db586bd9a31cc3e5e1f2
dhee9000 <dhee9000@gmail.com>
2020-07-21 01:01:54 -0500

    updated styling of instructions modal

--- src/react/minute/components/InstructionsModal.js
+++ src/react/minute/components/InstructionsModal.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, TouchableOpacity, Modal, Dimensions } from 'react-native';
 
 import { Text } from '../../common/components';
@@ -6,23 +6,28 @@ import { Colors, Fonts } from '../../../config';
 
 import { Icon } from 'react-native-elements';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 const { height, width } = Dimensions.get('screen');
 
 const InstructionsModal = (props) => {
 
     return (
         <Modal visible={props.showModal} transparent animated animationType={'slide'}>
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, }}>
-            <View style={{ backgroundColor: Colors.text, justifyContent: 'flex-start', alignItems: 'center', borderRadius: 16.0, elevation: 4.0, marginHorizontal: 16.0, padding: 16.0 }}>
-                <TouchableOpacity onPress={() => props.onClose()}>
-                    <Icon name={'close'} size={32} color={Colors.primary} />
-                </TouchableOpacity>
-                <Text style={{ color: '#f55' }}>Swipe Left if you are not interested</Text>
-                <Text style={{ color: '#5f5' }}>Swipe Right if you are interested</Text>
-                <Text style={{ color: '#55f' }}>Swipe Down to extend your time</Text>
-                <Text style={{ color: Colors.primary }}>Have a flipping amazing time :)</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, }}>
+                <View style={{ backgroundColor: Colors.background, justifyContent: 'flex-start', alignItems: 'center', borderRadius: 16.0, elevation: 4.0, marginHorizontal: 8.0, padding: 32.0 }}>
+                    <Text style={{ fontFamily: Fonts.heading, color: Colors.primary, fontSize: 24.0 }}>Starting Call</Text>
+                    <Text style={{ color: Colors.text }}>Swipe Left if you are not interested</Text>
+                    <Text style={{ color: Colors.text }}>Swipe Right if you are interested</Text>
+                    <Text style={{ color: Colors.text }}>Swipe Down to extend your time</Text>
+                    <Text style={{ color: Colors.text }}>Have a flipping amazing time :)</Text>
+                    <TouchableOpacity onPress={() => props.onClose()} style={{ alignSelf: 'stretch', marginVertical: 32.0 }}>
+                        <LinearGradient style={{ margin: 2.0, paddingVertical: 8.0, borderRadius: 28.0, height: 48, justifyContent: 'center', alignItems: 'center', width: '100%' }} colors={[Colors.primaryDark, Colors.primary]}>
+                            <Text style={{ fontFamily: Fonts.heading, color: Colors.background }}>Got It</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
         </Modal>
     );
 
