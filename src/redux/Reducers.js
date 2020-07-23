cb1555d58c1566f3d5c6428fb9b7daabc775452f
dhee9000 <dhee9000@gmail.com>
2020-07-23 15:37:47 -0500

    add state reset ability to app

--- src/redux/Reducers.js
+++ src/redux/Reducers.js
@@ -4,12 +4,20 @@ import { MatchesReducer } from './matches/reducers';
 import { ChatsReducer } from './chats/reducers';
 import { FiltersReducer } from './filters/reducers';
 
-const Reducers = combineReducers({
+const allReducers = combineReducers({
     __default: (state={}, action) => ({}),
     profiles: ProfilesReducer,
     matches: MatchesReducer,
     chats: ChatsReducer,
     filters: FiltersReducer,
-})
+});
+
+const Reducers = (state, action) => {
+    if(action.type == '@@RESET'){
+        state = undefined;
+    }
+
+    return allReducers(state, action);
+}
 
 export default Reducers;
